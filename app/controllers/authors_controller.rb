7663086bd7b788de57c6dec86fed5c7011b6e724
Petya Djanovska <petya.djanovska@gmail.com>
2018-07-06 02:19:25 +0000

    automatically backed up by learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -10,6 +10,9 @@ class AuthorsController < ApplicationController
     @author = Author.create!(author_params)
 
     redirect_to author_path(@author)
+    if !@post.valid?
+      render :new
+    end
   end
 
   private
